675504bedcc768e80122e2dce36d873e3c5a88bd
Deividas Gailius <deividasgailius86@gmail.com>
2023-11-06 19:35:45 +0200

    change name

--- func_one.py
+++ func_one.py
@@ -1,13 +1,7 @@
-def sum(a: int, b: int) -> int:
+def sumyte(a: int, b: int) -> int:
     return a + b
 
 def multiple(a: int, b: int) -> int:
     return a * b
 
 
-from calc import add
-
-a = 10
-b = 20
-result = add(a, b)
-print(result)
